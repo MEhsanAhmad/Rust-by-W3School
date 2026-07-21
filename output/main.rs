05c21e7f1292ed2ce5d8c9eff386de70d2aa48cb
M Ehsan Ahmad <mehsanahmad9290@gmail.com>
2026-07-22 00:27:49 +0500

    learn output

---
+++ output/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    line1();
+    line2();
+    line3();
+}
+
+fn line1() {
+    println!("My first line \n that is check n work or not");
+}
+
+fn line2() {
+    print!("this is my 2 line ");
+    print!("with write 2 line \n");
+}
+
+fn line3() {
+    print!("my name \n my projects")
+}
