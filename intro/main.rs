ac0941011997725f7c37cb5bbd74c3e16f6483c7
M Ehsan Ahmad <mehsanahmad9290@gmail.com>
2026-07-21 23:54:38 +0500

    intro 2

---
+++ intro/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    hello();
+    name();
+    city_name();
+    cname();
+}
+
+fn hello() {
+    println!("Hello World!");
+}
+
+fn name() {
+    println!("M Ehsan Ahmad");
+}
+
+fn city_name() {
+    println!("Lahore");
+}
+
+fn cname() {
+    println!("Pakistan");
+}
+
